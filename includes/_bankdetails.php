9b7f6d7e3e65e72eb772c9af74e5167fb3eea34a
mr-kyle <kyle@instil.org.au>
2018-05-12 21:20:19 +1000

    bank details update

--- includes/_bankdetails.php
+++ includes/_bankdetails.php
@@ -1,6 +1,6 @@
 ﻿<ul id="bankdetails">
     <li>Bank: ANZ</li>
-    <li>Vietnamese Evangelical Church Melbourne</li>
+    <li>Account name: VECA Melbourne</li>
     <li>BSB: 013265</li>
     <li>Account number: 401542337</li>
 </ul> 
